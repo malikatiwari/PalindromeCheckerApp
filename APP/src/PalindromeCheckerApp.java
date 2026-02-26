7e32f5e84844a0476816a98ca1f05aca07b19ebb
malikatiwari <malikatiwari1407@gmail.com>
2026-02-26 10:28:56 +0530

    checking palindrome

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String word = "madam";
+        String word = "wowzie";
         String reversed = "";
         for (int i = word.length() - 1; i >= 0; i--) {
             reversed = reversed + word.charAt(i);
